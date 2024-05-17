423bccd34c610d26816e4037a6e8dcd5111705b4
lennedy <lennedy@gmail.com>
2024-05-17 10:50:42 -0300

    mudança no estado ON/OFF.
    Relaciona ligado (ON) ou desligado (OFF) ao valor lido pelo sensor de corrente. Se a corrente lida for maior que 1A, cosidera-se que o motor está ligado.

--- AtuadorBaseMQTT.ino
+++ AtuadorBaseMQTT.ino
@@ -1,14 +1,14 @@
 #include "EspMQTTClient.h"
 #include <ArduinoJson.h>
 #include "EmonLib.h"
-#include "ConnectDataCell.h"
+#include "ConnectDataIFRN.h"
 
 
-#define DATA_INTERVAL 1000       // Intervalo para adquirir novos dados do sensor (milisegundos).
-                                 // Os dados serão publidados depois de serem adquiridos valores equivalentes a janela do filtro
+#define DATA_INTERVAL 10000       // Intervalo para adquirir novos dados do sensor (milisegundos).
+// Os dados serão publidados depois de serem adquiridos valores equivalentes a janela do filtro
 #define AVAILABLE_INTERVAL 5000  // Intervalo para enviar sinais de available (milisegundos)
 #define LED_INTERVAL_MQTT 1000        // Intervalo para piscar o LED quando conectado no broker
-#define JANELA_FILTRO 10         // Número de amostras do filtro para realizar a média
+#define JANELA_FILTRO 1         // Número de amostras do filtro para realizar a média
 
 byte ACIONAMENTO_PIN = 12;
 byte CONTROLE_SISTEMA_PIN = 14;
@@ -17,7 +17,7 @@ byte CONTROLE_SISTEMA_PIN = 14;
 unsigned long dataIntevalPrevTime = 0;      // will store last time data was send
 unsigned long availableIntevalPrevTime = 0; // will store last time "available" was send
 
-EnergyMonitor emon1; 
+EnergyMonitor emon1;
 
 void setup()
 {
@@ -26,112 +26,115 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ACIONAMENTO_PIN, OUTPUT); // Sets the trigPin as an Output
   pinMode(CONTROLE_SISTEMA_PIN, OUTPUT); // Sets the echoPin as an Input
-  
+
+  emon1.current(0, 30 / (3 * 0.75));
+
   // Optional functionalities of EspMQTTClient
   //client.enableMQTTPersistence();
   client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
   client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
   client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
-  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+  client.enableLastWillMessage("/bomba1/available", "offline");  // You can activate the retain flag by setting the third parameter to true
   //client.setKeepAlive(8);
   WiFi.mode(WIFI_STA);
 }
 
-void atuador(const String payload){
-  
-  if(payload == "ON"){
+void atuador(const String payload) {
+
+  if (payload == "ON") {
     digitalWrite(ACIONAMENTO_PIN, HIGH); //Liga o dispositivo
-    client.publish(topic_name, "ON");
+        client.publish(topic_name, "ON");
   }
-  else{
+  else {
     digitalWrite(ACIONAMENTO_PIN, LOW); //Desliga o dispositivo
-    client.publish(topic_name, "OFF");    
+        client.publish(topic_name, "OFF");
   }
 }
 
-void controleLocal(const String payload){
-  if(payload == "ON"){
+void controleLocal(const String payload) {
+  if (payload == "ON") {
     digitalWrite(CONTROLE_SISTEMA_PIN, HIGH); //Controle pelo sistema
-    client.publish(topic_name+"/controle_sistema", "ON");
+    client.publish(topic_name + "/controle_sistema", "ON");
   }
-  else{
+  else {
     digitalWrite(CONTROLE_SISTEMA_PIN, LOW); //Controle local
-    client.publish(topic_name+"/controle_sistema", "OFF");    
+    client.publish(topic_name + "/controle_sistema", "OFF");
   }
 }
 
 void onConnectionEstablished()
-{ 
+{
   client.subscribe(topic_name + "/set", atuador);
-  client.subscribe(topic_name + "/controle_sistema/set", controleLocal);  
-  
+  client.subscribe(topic_name + "/controle_sistema/set", controleLocal);
+
   availableSignal();
 }
 
-void availableSignal(){
-  client.publish(topic_name + "/available", "online"); 
+void availableSignal() {
+  client.publish(topic_name + "/available", "online");
 }
 
-float readSensor(){
-  double Irms = emon1.calcIrms(1480)/3.0;
-  
+float readSensor() {
+  double Irms = emon1.calcIrms(1480);
+
   return Irms;
 }
 
-void metodoPublisher(){
+void metodoPublisher() {
   static unsigned int amostras = 0;  //variável para realizar o filtro de média
   static float acumulador = 0;       //variável para acumular a média
-  
+
   acumulador += readSensor();
 
 
-  if (amostras >= JANELA_FILTRO){
+  if (amostras >= JANELA_FILTRO) {
     StaticJsonDocument<300> jsonDoc;
-  
+    float corrente = acumulador / JANELA_FILTRO;
+
     jsonDoc["RSSI"] = WiFi.RSSI();
-    jsonDoc["corrente"] = acumulador/JANELA_FILTRO;
-    
-    if(digitalRead(ACIONAMENTO_PIN) == HIGH){
+    jsonDoc["corrente"] = corrente;
+
+    if (corrente >= 1 ) {
       jsonDoc["estado"] = "ON";
     }
-    else{
+    else {
       jsonDoc["estado"] = "OFF";
     }
-  
+
     String payload = "";
     serializeJson(jsonDoc, payload);
 
-    client.publish(topic_name, payload); 
-    amostras = 0; 
+    client.publish(topic_name, payload);
+    amostras = 0;
     acumulador = 0;
 
-    if(digitalRead(CONTROLE_SISTEMA_PIN) == HIGH)
-      client.publish(topic_name+"/controle_sistema", "ON");
+    if (digitalRead(CONTROLE_SISTEMA_PIN) == HIGH)
+      client.publish(topic_name + "/controle_sistema", "ON");
     else
-      client.publish(topic_name+"/controle_sistema", "OFF");    
+      client.publish(topic_name + "/controle_sistema", "OFF");
   }
-  amostras++;  
+  amostras++;
 }
 
-void blinkLed(){
+void blinkLed() {
   static unsigned long ledWifiPrevTime = 0;
   static unsigned long ledMqttPrevTime = 0;
   unsigned long time_ms = millis();
   bool ledStatus = false;
-  
-  if( (WiFi.status() == WL_CONNECTED)){
-    if(client.isMqttConnected()){
-      if( (time_ms-ledMqttPrevTime) >= LED_INTERVAL_MQTT){
+
+  if ( (WiFi.status() == WL_CONNECTED)) {
+    if (client.isMqttConnected()) {
+      if ( (time_ms - ledMqttPrevTime) >= LED_INTERVAL_MQTT) {
         ledStatus = !digitalRead(LED_BUILTIN);
         digitalWrite(LED_BUILTIN, ledStatus);
         ledMqttPrevTime = time_ms;
-      }      
+      }
     }
-    else{
+    else {
       digitalWrite(LED_BUILTIN, LOW); //liga led
     }
   }
-  else{
+  else {
     digitalWrite(LED_BUILTIN, HIGH); //desliga led
   }
 }
@@ -143,7 +146,7 @@ void loop()
   client.loop();
 
   if (time_ms - dataIntevalPrevTime >= DATA_INTERVAL) {
-    client.executeDelayed(1 * 100, metodoPublisher);  
+    client.executeDelayed(1 * 100, metodoPublisher);
     dataIntevalPrevTime = time_ms;
   }
 
