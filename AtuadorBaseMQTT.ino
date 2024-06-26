31e407652298291b41b983e48ce5bc5126737062
lennedy <lennedy@gmail.com>
2024-06-26 01:40:18 -0300

    Firmware para novo dispositivo
    Adição de leitura do sensor leitor de NFC/RFId

--- AtuadorBaseMQTT.ino
+++ AtuadorBaseMQTT.ino
@@ -1,8 +1,13 @@
 #include "EspMQTTClient.h"
 #include <ArduinoJson.h>
-#include "EmonLib.h"
-#include "ConnectDataIFRN.h"
+#include "ConnectDataCell.h"
 
+#define NDEF_DEBUG false
+#include <Arduino.h>
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
 
 #define DATA_INTERVAL 10000       // Intervalo para adquirir novos dados do sensor (milisegundos).
 // Os dados serão publidados depois de serem adquiridos valores equivalentes a janela do filtro
@@ -10,24 +15,28 @@
 #define LED_INTERVAL_MQTT 1000        // Intervalo para piscar o LED quando conectado no broker
 #define JANELA_FILTRO 1         // Número de amostras do filtro para realizar a média
 
+//declaracao de objetos para uso na 
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
+
 byte ACIONAMENTO_PIN = 12;
-byte CONTROLE_SISTEMA_PIN = 14;
+byte BUZZER_PIN = 14;
 
 
 unsigned long dataIntevalPrevTime = 0;      // will store last time data was send
 unsigned long availableIntevalPrevTime = 0; // will store last time "available" was send
 
-EnergyMonitor emon1;
+String idChave;
 
 void setup()
 {
   Serial.begin(115200);
 
+  nfc.begin(); //inicia o leitor de nfc/rfid
+  
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ACIONAMENTO_PIN, OUTPUT); // Sets the trigPin as an Output
-  pinMode(CONTROLE_SISTEMA_PIN, OUTPUT); // Sets the echoPin as an Input
-
-  emon1.current(0, 30 / (3 * 0.75));
+  pinMode(BUZZER_PIN, OUTPUT); // Sets the echoPin as an Input
 
   // Optional functionalities of EspMQTTClient
   //client.enableMQTTPersistence();
@@ -51,21 +60,9 @@ void atuador(const String payload) {
   }
 }
 
-void controleLocal(const String payload) {
-  if (payload == "ON") {
-    digitalWrite(CONTROLE_SISTEMA_PIN, HIGH); //Controle pelo sistema
-    client.publish(topic_name + "/controle_sistema", "ON");
-  }
-  else {
-    digitalWrite(CONTROLE_SISTEMA_PIN, LOW); //Controle local
-    client.publish(topic_name + "/controle_sistema", "OFF");
-  }
-}
-
 void onConnectionEstablished()
 {
   client.subscribe(topic_name + "/set", atuador);
-  client.subscribe(topic_name + "/controle_sistema/set", controleLocal);
 
   availableSignal();
 }
@@ -74,46 +71,41 @@ void availableSignal() {
   client.publish(topic_name + "/available", "online");
 }
 
-float readSensor() {
-  double Irms = emon1.calcIrms(1480);
-
-  return Irms;
+bool readSensor() {
+  
+  if (nfc.tagPresent(10)){
+    NfcTag tag = nfc.read();
+    idChave = tag.getUidString();
+    return true;
+  }
+  return false;
 }
 
 void metodoPublisher() {
-  static unsigned int amostras = 0;  //variável para realizar o filtro de média
-  static float acumulador = 0;       //variável para acumular a média
 
-  acumulador += readSensor();
+  StaticJsonDocument<300> jsonDoc;
 
+  jsonDoc["RSSI"] = WiFi.RSSI();
 
-  if (amostras >= JANELA_FILTRO) {
-    StaticJsonDocument<300> jsonDoc;
-    float corrente = acumulador / JANELA_FILTRO;
+  String payload = "";
+  serializeJson(jsonDoc, payload);
 
-    jsonDoc["RSSI"] = WiFi.RSSI();
-    jsonDoc["corrente"] = corrente;
+  client.publish(topic_name, payload);
+}
 
-    if (corrente >= 1 ) {
-      jsonDoc["estado"] = "ON";
-    }
-    else {
-      jsonDoc["estado"] = "OFF";
-    }
+void sendChave(){
 
-    String payload = "";
-    serializeJson(jsonDoc, payload);
+  StaticJsonDocument<300> jsonDoc;
 
-    client.publish(topic_name, payload);
-    amostras = 0;
-    acumulador = 0;
+  jsonDoc["RSSI"] = WiFi.RSSI();
+  jsonDoc["idChave"] = idChave;
+  jsonDoc["idFechadura"] = client.getMqttClientName();
 
-    if (digitalRead(CONTROLE_SISTEMA_PIN) == HIGH)
-      client.publish(topic_name + "/controle_sistema", "ON");
-    else
-      client.publish(topic_name + "/controle_sistema", "OFF");
-  }
-  amostras++;
+  String payload = "";
+  serializeJson(jsonDoc, payload);
+
+  client.publish(topic_name, payload);
+
 }
 
 void blinkLed() {
@@ -142,7 +134,7 @@ void blinkLed() {
 void loop()
 {
   unsigned long time_ms = millis();
-
+  
   client.loop();
 
   if (time_ms - dataIntevalPrevTime >= DATA_INTERVAL) {
@@ -153,6 +145,10 @@ void loop()
   if (time_ms - availableIntevalPrevTime >= AVAILABLE_INTERVAL) {
     client.executeDelayed(1 * 500, availableSignal);
     availableIntevalPrevTime = time_ms;
+  }
+
+  if(readSensor()){
+    client.executeDelayed(1 * 100, sendChave);
   }
 
   blinkLed();
